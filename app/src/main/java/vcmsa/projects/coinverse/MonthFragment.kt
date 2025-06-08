158a427744792965ba7532ef4a1ab444f25e1458
Ashish <128583121+ash0github@users.noreply.github.com>
2025-06-08 13:21:37 +0200

    Month Filtering
    Configured month filtering for Expenses and Budget Goals. Made changes to the Month Fragment. such that the Tab Layout passes through the correct index values to the relevant pages for the relevant, selected month.

--- app/src/main/java/vcmsa/projects/coinverse/MonthFragment.kt
+++ app/src/main/java/vcmsa/projects/coinverse/MonthFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
 
 class MonthFragment : Fragment() {
+
+    private lateinit var monthTabLayout: TabLayout
+    private val months = listOf("January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December")
+
+    var onMonthSelected: ((Int) -> Unit)? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,16 +22,38 @@ class MonthFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_month, container, false)
 
-        val monthTabLayout = view.findViewById<TabLayout>(R.id.monthTabLayout)
+        monthTabLayout = view.findViewById<TabLayout>(R.id.monthTabLayout)
 
-        val months = listOf("January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December")
+        months.forEach { month -> monthTabLayout.addTab(monthTabLayout.newTab().setText(month)) }
 
-        for (month in months) {
-            monthTabLayout.addTab(monthTabLayout.newTab().setText(month))
-        }
+        monthTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.position?.let {index -> onMonthSelected?.invoke(index)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+
+        selectCurrentMonth()
+
+        //Old redundant code
+//        val months = listOf("January", "February", "March", "April", "May", "June",
+//            "July", "August", "September", "October", "November", "December")
+//
+//        for (month in months) {
+//            monthTabLayout.addTab(monthTabLayout.newTab().setText(month))
+//        }
 
         return view
+    }
+
+    //Sets and selects the current month by default
+    fun selectCurrentMonth() {
+        val currentMonthIndex = java.util.Calendar.getInstance().get(java.util.Calendar.MONTH)
+        monthTabLayout.getTabAt(currentMonthIndex)?.select()
+        onMonthSelected?.invoke(currentMonthIndex)
     }
 }
 
