c3a4b899ad5317f7531fd69ff99a673efafa7d4d
Ashish <128583121+ash0github@users.noreply.github.com>
2025-06-08 22:06:58 +0200

    Charting init
    Set up gradle files for charting.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
 
 kapt {
